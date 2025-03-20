dc0b0871e443488e9de970d39ec173622ca8aa75
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-19 21:39:23 -0300

    arquivo python

--- Python/aula01/pseudo.py
+++ Python/aula01/pseudo.py
@@ -6,15 +6,14 @@ reais = 0
 cotacao_dolar = 0
 conversao = 0
 
-print('Bem vindos ao Programa para converter reais em dólares!!!\n')
+print('Bem vindos ao Programa para converter reais em dolares!!!\n')
 reais = float(input("Digite o valor que você quer converter (Em reais): "))
 cotacao_dolar = float(input("Digite o valor da cotação do dólar hoje: \n"))
 
 conversao = reais/cotacao_dolar
 
 print("Com esta quantia é possível comprar? ")
-print(conversao)
+print(round(conversao,2))
 print(' dolares')
 
 print('Obrigado por usar nosso programa!!')
-
